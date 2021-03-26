de8a501972889edb4f345bc78d4e1ded2545e3b6
ohexus <moskalenko.taras.d@gmail.com>
2021-03-26 16:21:52 +0200

    updated index.ts for utils/

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,4 @@
+export { chalkColored } from './chalkColored';
 export { capitalizeFirstLetter } from './capitalizeFirstLetter';
+export { juicyFiglet } from './juicyFiglet';
 export { writeData } from './writeData';
