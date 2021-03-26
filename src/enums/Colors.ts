0c9db0de4e372fa11b8ade439e05eb3cce081b8f
ohexus <moskalenko.taras.d@gmail.com>
2021-03-26 16:18:04 +0200

    added Colors enum

---
+++ src/enums/Colors.ts
@@ -0,0 +1,7 @@
+export enum Colors {
+  Red = '#c62828',
+  Green = '#8bc34a',
+  Blue = '#2196f3',
+  Cyan = '#80deea',
+  Yellow = '#fdd835',
+}
--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,3 +1,4 @@
+export { Colors } from './Colors';
 export { ProgLangNames, ProgLangExts } from './ProgLangs';
 export { StyleLangNames, StyleLangExts } from './StyleLangs';
 export { TestLibNames, TestLibExts } from './TestLibs';
