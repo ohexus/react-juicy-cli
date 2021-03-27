c5680e9d94390bd0427e515d286c795d2d60df24
ohexus <moskalenko.taras.d@gmail.com>
2021-03-27 22:29:54 +0200

    updated index for templates

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,5 +1,14 @@
 export { indexTemplate, jsTemplate, tsTemplate } from './components';
-export { contextTsTemplate, contextTsTypesTemplate, providerTsTemplate, reducerTsTemplate } from './contexts';
+export {
+  contextJsTemplate,
+  contextTsTemplate,
+  contextJsTypesTemplate,
+  contextTsTypesTemplate,
+  providerJsTemplate,
+  providerTsTemplate,
+  reducerJsTemplate,
+  reducerTsTemplate,
+} from './contexts';
 export { hookJsTemplate, hookTsTemplate } from './hooks';
 export { cssTemplate, sassTemplate } from './stylesheets';
 export { testEnzymeTemplate, testTestingLibraryTemplate } from './testLibs';
