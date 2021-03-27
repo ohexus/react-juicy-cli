aef94669320b069e695cf8e801a69e2f0c25a5a5
ohexus <moskalenko.taras.d@gmail.com>
2021-03-27 17:58:06 +0200

    added GenerationEntity enum

---
+++ src/enums/GenerationEntities.ts
@@ -0,0 +1,4 @@
+export enum GenerationEntities {
+  Component = 'component',
+  Hook = 'hook',
+};
--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,4 +1,5 @@
 export { Colors } from './Colors';
+export { GenerationEntities } from './GenerationEntities';
 export { ProgLangNames, ProgLangExts } from './ProgLangs';
 export { StyleLangNames, StyleLangExts } from './StyleLangs';
 export { TestLibNames, TestLibExts } from './TestLibs';
