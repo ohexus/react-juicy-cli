fdafb2ede9243d5ac61c32c5397103e602e35524
ohexus <moskalenko.taras.d@gmail.com>
2021-03-27 22:39:56 +0200

    added missing exports

--- src/templates/contexts/contextJsTypesTemplate.ts
+++ src/templates/contexts/contextJsTypesTemplate.ts
@@ -1,4 +1,6 @@
-export const contextJsTypesTemplate = (name: string = 'ContextTemplate'): string => `const CLEAR_STATE = 'CLEAR_STATE';
-const SET_STATUS = 'SET_STATUS';
-const TOGGLE_STATUS = 'TOGGLE_STATUS';
+export const contextJsTypesTemplate = (
+  name: string = 'ContextTemplate',
+): string => `export const CLEAR_STATE = 'CLEAR_STATE';
+export const SET_STATUS = 'SET_STATUS';
+export const TOGGLE_STATUS = 'TOGGLE_STATUS';
 `;
