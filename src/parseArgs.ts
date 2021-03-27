00da44861274e6e6805f696d17c9b469d9276022
ohexus <moskalenko.taras.d@gmail.com>
2021-03-27 21:49:27 +0200

    added --context flag, added error message if several one typed flags provided

--- src/parseArgs.ts
+++ src/parseArgs.ts
@@ -4,13 +4,31 @@ import { config } from './config';
 import { capitalizeFirstLetter, replaceWithUse } from './utils';
 
 import { GenerationEntities, ProgLangNames, StyleLangNames, TestLibNames } from './enums';
-import { ComponentConfigBasic, HookConfigBasic } from './interfaces';
+import { ComponentConfigBasic, ContextConfigBasic, HookConfigBasic } from './interfaces';
+import { generateComponent, generateContext, generateHook } from './commands';
 
-export function parseArgs(rawArgs: string[]): void {
+function severalFlagsMessage(flags: string[]) {
+  const message = flags.reduce((acc, next, index) => {
+    if (index !== 0) {
+      if (index < flags.length - 1) {
+        acc += ', ';
+      } else {
+        acc += ' and ';
+      }
+    }
+
+    return (acc += next);
+  }, '');
+
+  return message + ' flags can only be used separately!';
+}
+
+export async function parseArgs(rawArgs: string[]): Promise<void> {
   const args = arg(
     {
       // Flags
       '--component': String,
+      '--context': String,
       '--hook': String,
       '--javascript': Boolean,
       '--typescript': Boolean,
@@ -22,6 +40,7 @@ export function parseArgs(rawArgs: string[]): void {
       '--testing-library': Boolean,
       // Aliases
       '--cmp': '--component',
+      '--ctx': '--context',
       '--hk': '--hook',
       '--js': '--javascript',
       '--ts': '--typescript',
@@ -31,8 +50,33 @@ export function parseArgs(rawArgs: string[]): void {
     { permissive: true, argv: rawArgs },
   );
 
-  if (args['--component'] && args['--hook']) {
-    throw new Error("--component and --hook flags can't be used together");
+  const isSeveralEntities =
+    (args['--component'] && args['--hook']) ||
+    (args['--component'] && args['--context']) ||
+    (args['--hook'] && args['--context']);
+  if (isSeveralEntities) {
+    throw new Error(severalFlagsMessage(['--component', '--context', '--hook']));
+  }
+
+  const isSeveralProgLangs = args['--javascript'] && args['--typescript'];
+  if (isSeveralProgLangs) {
+    throw new Error(severalFlagsMessage(['--javascript', '--typescript']));
+  }
+
+  const isSeveralStyleLangs =
+    (args['--css'] && args['--scss']) ||
+    (args['--css'] && args['--sass']) ||
+    (args['--css'] && args['--less']) ||
+    (args['--scss'] && args['--sass']) ||
+    (args['--scss'] && args['--less']) ||
+    (args['--sass'] && args['--less']);
+  if (isSeveralStyleLangs) {
+    throw new Error(severalFlagsMessage(['--css', '--scss', '--sass', '--less']));
+  }
+
+  const isSeveralTestLibs = args['--enzyme'] && args['--testing-library'];
+  if (isSeveralTestLibs) {
+    throw new Error(severalFlagsMessage(['--enzyme', '--testing-library']));
   }
 
   const prog = (() => {
@@ -76,10 +120,21 @@ export function parseArgs(rawArgs: string[]): void {
       style,
       testLib,
     } as ComponentConfigBasic);
+
+    await generateComponent();
+  } else if (args['--context']) {
+    config.set(GenerationEntities.Context, {
+      name: capitalizeFirstLetter(args['--context']),
+      prog,
+    } as ContextConfigBasic);
+
+    await generateContext();
   } else if (args['--hook']) {
     config.set(GenerationEntities.Hook, {
       name: replaceWithUse(args['--hook']),
       prog,
     } as HookConfigBasic);
+
+    await generateHook();
   }
 }
