4d15715ae4b7ec164b7f7088cf71ec9c04cbad4a
ohexus <moskalenko.taras.d@gmail.com>
2021-03-30 17:57:28 +0300

    updated writeData with StatusMessages enum

--- src/utils/writeData.ts
+++ src/utils/writeData.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 
+import { StatusMessages } from '../enums';
 import { PromiseReturnStatus } from '../interfaces';
 
 export function writeData(path: string, data: string): Promise<PromiseReturnStatus> {
@@ -10,7 +11,7 @@ export function writeData(path: string, data: string): Promise<PromiseReturnStat
         reject(error);
       }
 
-      resolve('Success!');
+      resolve(StatusMessages.Success);
     });
   });
 }
