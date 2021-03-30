bd85419f4bb917616d8bace7e67c4f1a390f0029
ohexus <moskalenko.taras.d@gmail.com>
2021-03-30 21:30:04 +0300

    added Configs and Quotes enums

---
+++ src/enums/Quotes.ts
@@ -0,0 +1,4 @@
+export enum Quotes {
+  Double = '"',
+  Single = "'",
+}
--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,6 +1,8 @@
 export { Colors } from './Colors';
+export { Configs } from './Configs';
 export { GenerationEntities } from './GenerationEntities';
 export { ProgLangNames, ProgLangExts } from './ProgLangs';
+export { Quotes } from './Quotes';
 export { StatusMessages } from './StatusMessages';
 export { StyleLangNames, StyleLangExts } from './StyleLangs';
 export { TestLibNames, TestLibExts } from './TestLibs';
