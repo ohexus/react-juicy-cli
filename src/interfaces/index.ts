c85b6bc6d0b61368a5558464a25a0c5d80298228
ohexus <moskalenko.taras.d@gmail.com>
2021-03-30 21:29:40 +0300

    added GlobalConfig interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,5 @@
 export { ComponentConfig, ComponentConfigBasic } from './ComponentConfig';
 export { ContextConfig, ContextConfigBasic } from './ContextConfig';
+export { GlobalConfig } from './GlobalConfig';
 export { HookConfig, HookConfigBasic } from './HookConfig';
 export { PromiseReturnStatus } from './PromiseReturnStatus';
