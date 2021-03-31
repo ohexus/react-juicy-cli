509b56acebffad52da66dc0111ffdba417c291aa
ohexus <moskalenko.taras.d@gmail.com>
2021-03-31 09:37:49 +0300

    added logHelp and logVersion commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,3 +1,5 @@
 export { generateComponent } from './generateComponent';
 export { generateContext } from './generateContext';
 export { generateHook } from './generateHook';
+export { logHelp } from './logHelp';
+export { logVersion } from './logVersion';
---
+++ src/commands/logHelp.ts
@@ -0,0 +1,32 @@
+export function logHelp(): void {
+  console.log(`
+  Usage: react-juicy-cli [flags <string?>]
+
+  Available flags:
+
+  -h, --help                          display help for react-juicy-cli
+  -v, --version                       display current version
+  --cmp, --component <string>         specify component name and tell CLI to generate a component
+  --ctx, --context <string>           specify context name and tell CLI to generate a context
+  --hk, --hook <string>               specify hook name and tell CLI to generate a hook
+  --js, --javascript                  generate JavaScript component
+  --ts, --typescript                  generate TypeScript component
+  --css                               generate CSS file
+  --scss                              generate SCSS file
+  --sass                              generate SASS file
+  --less                              generate LESS file
+  --enz, --enzyme                     generate .spec file according to your component extension (tsx \| jsx)
+  --test-lib, --testing-library       generate .test file according to your component extension (tsx \| jsx)
+  --skipS, --skip-styles              skip styles step and generate component with tests
+  --skipT, --skip-tests               skip tests step and generate component with styles
+  --skip                              skip styles and tests and generate only component
+  --sq, --single-quotes               show the generator that you prefer single quotes
+  --dq, --double-quotes               show the generator that you prefer double quotes
+
+  Alternative flags:
+
+  --skip-styles:                      --skip-style, --skipStyle, --skipStyles,
+  --skip-tests:                       --skip-test, --skipTest, --skipTests,
+  --testing-library:                  --testing-lib, --testingLib, --testingLibrary
+`);
+}
