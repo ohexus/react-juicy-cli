0aab59062064ff19da37e475057c129bef34dc98
ohexus <moskalenko.taras.d@gmail.com>
2021-04-02 15:05:01 +0300

    added missing await

--- src/cli.ts
+++ src/cli.ts
@@ -35,7 +35,7 @@ export default async function cli(argv: string[]): Promise<void> {
 
       clear();
     } else {
-      parseArgs(args);
+      await parseArgs(args);
     }
 
     process.exit(0);
