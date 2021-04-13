75763f8afb486fa2283fb7432b88a100cd8c0fc4
ohexus <moskalenko.taras.d@gmail.com>
2021-04-13 17:43:01 +0300

    refactored test templates

---
+++ src/templates/testLibs/testingLibraryTemplate.ts
@@ -0,0 +1,17 @@
+const testingLibraryTemplate = (name: string): string => `import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import ${name} from './${name}';
+
+describe('${name}', () => {
+  it('should mount', () => {
+    render(<${name} />);
+    
+    const element = screen.getByTestId('${name}.testId');
+    expect(element).toBeInTheDocument();
+  });
+});
+`;
+
+export default testingLibraryTemplate;
