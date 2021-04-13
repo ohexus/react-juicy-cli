7bf64cf1a3dc7e1504bc5801a8dca2c24677a907
ohexus <moskalenko.taras.d@gmail.com>
2021-04-13 17:24:28 +0300

    added askTestType question

---
+++ src/questions/askTestType.ts
@@ -0,0 +1,15 @@
+import inquirer from 'inquirer';
+
+import { TestTypes } from '../enums';
+import { ComponentConfig } from '../interfaces';
+
+const testTypeQuestion = {
+  name: 'testType',
+  type: 'list',
+  message: 'Which testing library do you want to use?',
+  choices: Object.values(TestTypes),
+};
+
+export async function askTestType(): Promise<ComponentConfig['testType']> {
+  return (await inquirer.prompt([testTypeQuestion])).testType;
+}
