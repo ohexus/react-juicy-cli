72f9b34e56e91cfcb367517f3ee0f3c700e2d657
ohexus <moskalenko.taras.d@gmail.com>
2021-04-13 18:11:29 +0300

    added export to Props interface

--- src/templates/components/tsTemplate.ts
+++ src/templates/components/tsTemplate.ts
@@ -4,7 +4,7 @@ import { Quotes, StyleLangs } from '../../enums';
 
 const tsTemplate = (name: string, cssExt: StyleLangs, quotes: Quotes): string => `import React from 'react';
 ${cssImportTemplate(name, cssExt)}
-interface ${name}Props {}
+export interface ${name}Props {}
 
 const ${name}: React.FC<${name}Props> = ({}) => (
   <div className=${quotes}${name.toLowerCase()}${quotes} data-testid=${quotes}${name}.testId${quotes}>
