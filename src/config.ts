9711694c40b888cf453e3243ff7dfb35d30d5223
ohexus <moskalenko.taras.d@gmail.com>
2021-04-15 09:15:23 +0300

    set default configs

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,33 @@
 import Configstore from 'configstore';
+import { Configs } from './enums';
 
-export default new Configstore('react-juicy-cli');
+const config = new Configstore('react-juicy-cli');
+
+config.set(Configs.Global, {
+  entity: null,
+  prog: null,
+  skipStyles: false,
+  skipTests: false,
+  quotes: null,
+});
+
+config.set(Configs.Component, {
+  style: null,
+  name: null,
+});
+
+config.set(Configs.Context, {
+  name: null,
+});
+
+config.set(Configs.Hook, {
+  name: null,
+});
+
+config.set(Configs.Test, {
+  lib: null,
+  name: null,
+  type: null,
+});
+
+export default config;
