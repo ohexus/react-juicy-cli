e3b4177025df5ac68140034df9ab496f94b81d68
ohexus <moskalenko.taras.d@gmail.com>
2021-04-15 16:11:33 +0300

    moved config.clear call after program termination

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import reactJuicyCLI from './cli';
+import config from './config';
 import { chalkColored, clearTerminal, juicyFiglet } from './utils';
 
 (async () => {
   clearTerminal();
   await juicyFiglet();
   await reactJuicyCLI(process.argv);
+  config.clear();
 })().catch((err: Error) => {
+  config.clear();
   console.log(chalkColored(`\n${err.message}\n`, 'Red'));
   process.exit(1);
 });
