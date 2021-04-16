846857dd3f9217ed039e4cd5ec6bfd12ba597e57
ohexus <moskalenko.taras.d@gmail.com>
2021-04-16 12:24:34 +0300

    added flag info constants

---
+++ src/constants/alternativeFlags.ts
@@ -0,0 +1,16 @@
+const ALTERNATIVE_FLAGS = [
+  {
+    flag: '--skip-styles',
+    alts: ['--skip-style', '--skipStyle', '--skipStyles'],
+  },
+  {
+    flag: '--skip-tests',
+    alts: ['--skip-test', '--skipTest', '--skipTests'],
+  },
+  {
+    flag: '--testing-library',
+    alts: ['--testing-lib', '--testingLib', '--testingLibrary'],
+  },
+];
+
+export default ALTERNATIVE_FLAGS;
---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export { default as ALTERNATIVE_FLAGS } from './alternativeFlags';
+export { default as FLAG_INFO } from './flagInfo';
