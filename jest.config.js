66cb7f2defed50215ae0859e1f04f2294856be1c
ohexus <moskalenko.taras.d@gmail.com>
2021-04-16 23:11:56 +0300

    added jest config

---
+++ jest.config.js
@@ -0,0 +1,47 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  cacheDirectory: '<rootDir>/.jestcache',
+
+  coverageDirectory: 'coverage',
+
+  coveragePathIgnorePatterns: ['/node_modules/'],
+
+  coverageReporters: [
+    // 'json',
+    ['text', { skipFull: true }],
+    'text-summary',
+    'lcov',
+    // 'clover',
+  ],
+
+  coverageThreshold: {
+    global: {
+      statements: 60,
+      branches: 60,
+      functions: 60,
+      lines: 60,
+    },
+  },
+
+  errorOnDeprecated: true,
+
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+
+  moduleDirectories: ['<rootDir>', 'node_modules', 'src'],
+
+  modulePaths: ['<rootDir>'],
+
+  preset: 'ts-jest',
+
+  resetMocks: true,
+
+  roots: ['<rootDir>'],
+
+  testMatch: ['**/*.spec.ts?(x)', '**/*.test.ts?(x)'],
+};
