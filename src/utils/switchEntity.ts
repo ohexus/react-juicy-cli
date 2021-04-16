6199a62f6b379fb81606d3f7625c80fceab68532
ohexus <moskalenko.taras.d@gmail.com>
2021-04-16 20:14:04 +0300

    added switchEntity method

---
+++ src/utils/switchEntity.ts
@@ -0,0 +1,27 @@
+import arg from 'arg';
+
+import { GenerationEntities, ProgLangNames, Quotes, StyleLangs, TestLibs, TestTypes } from '../enums';
+
+type Entities =
+  | typeof GenerationEntities
+  | typeof ProgLangNames
+  | typeof Quotes
+  | typeof StyleLangs
+  | typeof TestLibs
+  | typeof TestTypes;
+
+type ValueOf<T> = T[keyof T];
+
+function parseValues<EntityType>(entities: EntityType): ValueOf<EntityType> {
+  return (Object.keys(entities).map((k) => entities[k as keyof EntityType]) as unknown) as ValueOf<EntityType>;
+}
+
+export default function switchEntity<Args extends arg.Spec>(
+  args: arg.Result<Args>,
+  flags: string[],
+  entities: Entities,
+): string | null {
+  const index = flags.findIndex((flag) => args[flag]);
+
+  return index !== -1 ? parseValues(entities)[index] : null;
+}
