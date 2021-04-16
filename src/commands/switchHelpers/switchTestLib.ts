9e69d1316a04cfb429cab1fca1e53f010296b664
ohexus <moskalenko.taras.d@gmail.com>
2021-04-16 17:04:21 +0300

    increased number of templates returned by switchTestLib

--- src/commands/switchHelpers/switchTestLib.ts
+++ src/commands/switchHelpers/switchTestLib.ts
@@ -1,14 +1,45 @@
-import { enzymeTemplate, testingLibraryTemplate } from '../../templates';
+import {
+  enzymeComponentTemplate,
+  testingLibraryComponentTemplate,
+  enzymeHookTemplateJS,
+  enzymeHookTemplateTS,
+  testingLibraryHookTemplate,
+} from '../../templates';
 
-import { TestLibs } from '../../enums';
+import { GenerationEntities, ProgLangNames, TestLibs } from '../../enums';
 
-function switchTestLib(lib: TestLibs): typeof enzymeTemplate | typeof testingLibraryTemplate {
+type ComponentTemplateTypes = typeof enzymeComponentTemplate | typeof testingLibraryComponentTemplate;
+type HookTemplateTypes = typeof enzymeHookTemplateJS | typeof enzymeHookTemplateTS | typeof testingLibraryHookTemplate;
+
+function switchTemplateForComponent(lib: TestLibs): ComponentTemplateTypes {
   if (lib === TestLibs.Enzyme) {
-    return enzymeTemplate;
+    return enzymeComponentTemplate;
   } else if (lib === TestLibs.TestingLibrary) {
-    return testingLibraryTemplate;
+    return testingLibraryComponentTemplate;
   }
-  return enzymeTemplate;
+  return enzymeComponentTemplate;
+}
+
+function switchTemplateForHook(lib: TestLibs, lang: ProgLangNames): HookTemplateTypes {
+  if (lib === TestLibs.Enzyme) {
+    return lang === ProgLangNames.TS ? enzymeHookTemplateTS : enzymeHookTemplateJS;
+  } else if (lib === TestLibs.TestingLibrary) {
+    return testingLibraryHookTemplate;
+  }
+  return enzymeHookTemplateTS;
+}
+
+function switchTestLib(
+  lib: TestLibs,
+  entity: GenerationEntities,
+  lang: ProgLangNames,
+): ComponentTemplateTypes | HookTemplateTypes {
+  if (entity === GenerationEntities.Component) {
+    return switchTemplateForComponent(lib);
+  } else if (entity === GenerationEntities.Hook) {
+    return switchTemplateForHook(lib, lang);
+  }
+  return enzymeComponentTemplate;
 }
 
 export default switchTestLib;
