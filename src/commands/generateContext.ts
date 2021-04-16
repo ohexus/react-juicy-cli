a25fcf938cf10fa9f91e4aaf07b596876164e4db
ohexus <moskalenko.taras.d@gmail.com>
2021-04-16 13:31:23 +0300

    removed quotes from generateContext

--- src/commands/generateContext.ts
+++ src/commands/generateContext.ts
@@ -10,70 +10,70 @@ import {
 } from './switchHelpers';
 import { writeData } from '../utils';
 
-import { Configs, ProgLangNames, Quotes } from '../enums';
+import { Configs, ProgLangNames } from '../enums';
 import { ContextConfig, GlobalConfig, PromiseReturnStatus } from '../interfaces';
 
-function contextPromise(name: string, lang: ProgLangNames, quotes: Quotes): Promise<PromiseReturnStatus> {
+function contextPromise(name: string, lang: ProgLangNames): Promise<PromiseReturnStatus> {
   const ext = switchExt(lang);
   const template = switchContextTemplate(lang);
 
   return new Promise((resolve, reject) => {
-    writeData(`${name}/${name}.${ext}`, template(name, quotes))
+    writeData(`${name}/${name}.${ext}`, template(name))
       .then((status) => resolve(status))
       .catch((error) => reject(error));
   });
 }
 
-function contextTypesPromise(name: string, lang: ProgLangNames, quotes: Quotes): Promise<PromiseReturnStatus> {
+function contextTypesPromise(name: string, lang: ProgLangNames): Promise<PromiseReturnStatus> {
   const ext = switchExt(lang);
   const template = switchContextTypesTemplate(lang);
 
   return new Promise((resolve, reject) => {
-    writeData(`${name}/${name}Types.${ext}`, template(name, quotes))
+    writeData(`${name}/${name}Types.${ext}`, template(name))
       .then((status) => resolve(status))
       .catch((error) => reject(error));
   });
 }
 
-function providerPromise(name: string, lang: ProgLangNames, quotes: Quotes): Promise<PromiseReturnStatus> {
+function providerPromise(name: string, lang: ProgLangNames): Promise<PromiseReturnStatus> {
   const ext = switchExt(lang);
   const template = switchContextProviderTemplate(lang);
 
   return new Promise((resolve, reject) => {
-    writeData(`${name}/${name}Provider.${ext}x`, template(name, quotes))
+    writeData(`${name}/${name}Provider.${ext}x`, template(name))
       .then((status) => resolve(status))
       .catch((error) => reject(error));
   });
 }
 
-function reducerPromise(name: string, lang: ProgLangNames, quotes: Quotes): Promise<PromiseReturnStatus> {
+function reducerPromise(name: string, lang: ProgLangNames): Promise<PromiseReturnStatus> {
   const ext = switchExt(lang);
   const template = switchContextReducerTemplate(lang);
 
   return new Promise((resolve, reject) => {
-    writeData(`${name}/${name}Reducer.${ext}`, template(name, quotes))
+    writeData(`${name}/${name}Reducer.${ext}`, template(name))
       .then((status) => resolve(status))
       .catch((error) => reject(error));
   });
 }
 
-const getContextConfig = (): ContextConfig & { prog: GlobalConfig['prog']; quotes: GlobalConfig['quotes'] } => {
-  const { prog, quotes } = config.get(Configs.Global) as GlobalConfig;
+const getContextConfig = (): ContextConfig & { prog: GlobalConfig['prog'] } => {
+  const { prog } = config.get(Configs.Global) as GlobalConfig;
   const { name } = config.get(Configs.Context) as ContextConfig;
 
-  return { name, prog, quotes };
+  return { name, prog };
 };
 
 async function generateContext(): Promise<PromiseReturnStatus[]> {
-  const { name, prog, quotes } = getContextConfig();
+  const { name, prog } = getContextConfig();
 
   fs.mkdirSync(name, { recursive: true });
 
   return Promise.all([
-    contextPromise(name, prog, quotes),
-    contextTypesPromise(name, prog, quotes),
-    providerPromise(name, prog, quotes),
-    reducerPromise(name, prog, quotes),
+    contextPromise(name, prog),
+    contextTypesPromise(name, prog),
+    providerPromise(name, prog),
+    reducerPromise(name, prog),
   ]);
 }
 
