285d318cb0e99a50b9ed151babb45a826ca17dd0
ohexus <moskalenko.taras.d@gmail.com>
2021-04-18 19:24:11 +0300

    added unit test for switchContextTypesTemplate

---
+++ src/commands/switchHelpers/switchContextTypesTemplate/switchContextTypesTemplate.spec.ts
@@ -0,0 +1,39 @@
+import switchContextTypesTemplate from './switchContextTypesTemplate';
+
+import { ProgLangNames } from '../../../enums';
+import { contextJsTypesTemplate, contextTsTypesTemplate } from '../../../templates';
+
+jest.mock('../../../templates', () => ({
+  __esModule: true,
+  contextJsTypesTemplate: jest.fn(),
+  contextTsTypesTemplate: jest.fn(),
+}));
+
+describe('switchContextTypesTemplate', () => {
+  const NAME = 'foo';
+
+  const mockJsTemplate = 'foo';
+  const mockTsTemplate = 'bar';
+
+  beforeEach(() => {
+    (contextJsTypesTemplate as jest.Mock).mockImplementation(() => mockJsTemplate);
+    (contextTsTypesTemplate as jest.Mock).mockImplementation(() => mockTsTemplate);
+  });
+
+  it('switches template', () => {
+    const LANGS = [ProgLangNames.JS, ProgLangNames.TS];
+    const TEMPLATES = [mockJsTemplate, mockTsTemplate];
+
+    LANGS.forEach((lang, index) => {
+      const template = switchContextTypesTemplate(lang);
+
+      expect(template(NAME)).toEqual(TEMPLATES[index]);
+    });
+  });
+
+  it('returns default template', () => {
+    const template = switchContextTypesTemplate(('foo' as unknown) as ProgLangNames);
+
+    expect(template(NAME)).toEqual(mockTsTemplate);
+  });
+});
