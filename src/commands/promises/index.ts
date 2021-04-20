163adce41f56fee353f7155380a762ebc0707f77
ohexus <moskalenko.taras.d@gmail.com>
2021-04-20 23:15:57 +0300

    added Promises postfix to dirs

--- src/commands/promises/index.ts
+++ src/commands/promises/index.ts
@@ -1,4 +1,10 @@
-export { componentPromise, componentIndexPromise, styleSheetPromise } from './component';
-export { contextIndexPromise, contextPromise, contextTypesPromise, providerPromise, reducerPromise } from './context';
-export { hookIndexPromise, hookPromise } from './hook';
-export { testPromise } from './test';
+export { componentPromise, componentIndexPromise, styleSheetPromise } from './componentPromises';
+export {
+  contextIndexPromise,
+  contextPromise,
+  contextTypesPromise,
+  providerPromise,
+  reducerPromise,
+} from './contextPromises';
+export { hookIndexPromise, hookPromise } from './hookPromises';
+export { testPromise } from './testPromises';
