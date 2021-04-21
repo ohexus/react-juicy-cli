2c996a2895d42975760c44bad68006a38e7b12f5
ohexus <moskalenko.taras.d@gmail.com>
2021-04-21 19:19:55 +0300

    updated writeData unit tests

--- src/utils/writeData/writeData.spec.ts
+++ src/utils/writeData/writeData.spec.ts
@@ -8,16 +8,14 @@ describe('writeData', () => {
   const PATH = 'foo';
   const DATA = 'bar';
 
-  it('returns Success if data wrote', async () => {
+  it('returns success if data wrote', async () => {
     ((fs.writeFile as unknown) as jest.Mock).mockImplementation(
       (path: string, data: string, callback: () => void): void => {
         callback();
       },
     );
 
-    const status = await writeData(PATH, DATA);
-
-    expect(status).toEqual('Success');
+    await expect(writeData(PATH, DATA)).resolves.toEqual('Success');
     expect(fs.writeFile).toHaveBeenCalledTimes(1);
   });
 
@@ -30,8 +28,6 @@ describe('writeData', () => {
       },
     );
 
-    await expect(async () => {
-      await writeData(PATH, DATA);
-    }).rejects.toThrow(new Error(ERROR));
+    await expect(writeData(PATH, DATA)).rejects.toThrow(new Error(ERROR));
   });
 });
