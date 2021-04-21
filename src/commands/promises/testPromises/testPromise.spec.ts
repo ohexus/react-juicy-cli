4cbbbd61c38f6f5a143e6ba34fc83de17bdd4114
ohexus <moskalenko.taras.d@gmail.com>
2021-04-21 19:18:29 +0300

    added unit tests for testPromise

---
+++ src/commands/promises/testPromises/testPromise.spec.ts
@@ -0,0 +1,46 @@
+import mockWriteDataError from '../../../../__mocks__/mockWriteDataError';
+import mockWriteDataSuccess from '../../../../__mocks__/mockWriteDataSuccess';
+
+import testPromise from './testPromise';
+
+import { ProgLangNames, TestLibs, TestTypes, GenerationEntities } from '../../../enums';
+import { writeData } from '../../../utils';
+import { switchExt, switchTestExt, switchTestLibTemplate } from '../../switchHelpers';
+
+jest.mock('../../switchHelpers', () => ({
+  __esModule: true,
+  switchExt: jest.fn(),
+  switchTestExt: jest.fn(),
+  switchTestLibTemplate: jest.fn(),
+}));
+
+jest.mock('../../../utils', () => ({
+  __esModule: true,
+  writeData: jest.fn(),
+}));
+
+describe('testPromise', () => {
+  const name = 'corge';
+  const lang = ProgLangNames.TS;
+  const lib = TestLibs.Enzyme;
+  const type = TestTypes.Unit;
+  const entity = GenerationEntities.Test;
+
+  beforeEach(() => {
+    (switchExt as jest.Mock).mockReturnValue('qux');
+    (switchTestExt as jest.Mock).mockReturnValue('quux');
+    (switchTestLibTemplate as jest.Mock).mockReturnValue(jest.fn());
+  });
+
+  it('returns test template promise', async () => {
+    (writeData as jest.Mock).mockImplementation(mockWriteDataSuccess);
+
+    await expect(testPromise(name, lang, lib, type, entity)).resolves.toEqual('foo');
+  });
+
+  it('throws an error', async () => {
+    (writeData as jest.Mock).mockImplementation(mockWriteDataError);
+
+    await expect(testPromise(name, lang, lib, type, entity)).rejects.toEqual(new Error('bar'));
+  });
+});
