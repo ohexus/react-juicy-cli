abf4f24f05ac852bfbb5d52443389dd212568830
ohexus <moskalenko.taras.d@gmail.com>
2021-04-21 20:03:57 +0300

    added unit tests for component promises

---
+++ src/commands/promises/componentPromises/componentIndexPromise.spec.ts
@@ -0,0 +1,39 @@
+import { mockWriteDataError, ERROR } from '../../../../__mocks__/writeDataMocks/mockWriteDataError';
+import { mockWriteDataSuccess, SUCCESS } from '../../../../__mocks__/writeDataMocks/mockWriteDataSuccess';
+
+import componentIndexPromise from './componentIndexPromise';
+
+import { ProgLangNames } from '../../../enums';
+import { writeData } from '../../../utils';
+import { switchExt } from '../../switchHelpers';
+
+jest.mock('../../switchHelpers', () => ({
+  __esModule: true,
+  switchExt: jest.fn(),
+}));
+
+jest.mock('../../../utils', () => ({
+  __esModule: true,
+  writeData: jest.fn(),
+}));
+
+describe('componentIndexPromise', () => {
+  const name = 'quuz';
+  const lang = ProgLangNames.TS;
+
+  beforeEach(() => {
+    (switchExt as jest.Mock).mockReturnValue('qux');
+  });
+
+  it('creates a file with component index template', async () => {
+    (writeData as jest.Mock).mockImplementation(mockWriteDataSuccess);
+
+    await expect(componentIndexPromise(name, lang)).resolves.toEqual(SUCCESS);
+  });
+
+  it('throws an error', async () => {
+    (writeData as jest.Mock).mockImplementation(mockWriteDataError);
+
+    await expect(componentIndexPromise(name, lang)).rejects.toEqual(ERROR);
+  });
+});
