509c7364da7a3920493b836d5814932b5d52e67f
ohexus <moskalenko.taras.d@gmail.com>
2021-04-24 17:46:40 +0300

    added unit tests for generate commands

---
+++ src/commands/generateComponent.spec.ts
@@ -0,0 +1,112 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+import { mockRejectedPromises, mockResolvedPromises } from '../../__mocks__/mockGenerationPromises';
+import config from '../config';
+
+import generateComponent from './generateComponent';
+import { componentIndexPromise, componentPromise, styleSheetPromise } from './promises';
+
+import { Configs } from '../enums';
+
+jest.mock('../config', () => ({
+  __esModule: true,
+  default: { get: jest.fn() },
+}));
+
+jest.mock('./promises', () => ({
+  __esModule: true,
+  componentIndexPromise: jest.fn(),
+  componentPromise: jest.fn(),
+  styleSheetPromise: jest.fn(),
+}));
+
+jest.mock('../utils', () => ({
+  __esModule: true,
+  makeDir: jest.fn(),
+}));
+
+const promises = [componentIndexPromise, componentPromise, styleSheetPromise];
+
+describe('generateComponent', () => {
+  const name = 'foo';
+  const style = 'bar';
+  const prog = 'baz';
+  const quotes = 'qux';
+
+  const promiseSuccess = 'corge';
+  const promiseError = 'waldo';
+
+  const globalConfig = {
+    prog,
+    quotes,
+    skipStyles: false,
+  };
+
+  const componentConfig = {
+    name,
+    style,
+  };
+
+  it('generates component with styles', async () => {
+    (config.get as jest.Mock).mockReturnValueOnce(globalConfig).mockReturnValueOnce(componentConfig);
+
+    mockResolvedPromises(promises, promiseSuccess);
+
+    await expect(generateComponent()).resolves.toEqual([promiseSuccess, promiseSuccess, promiseSuccess]);
+
+    expect(config.get).toHaveBeenCalledTimes(2);
+    expect(config.get).toHaveBeenNthCalledWith(1, Configs.Global);
+    expect(config.get).toHaveBeenNthCalledWith(2, Configs.Component);
+
+    expect(componentIndexPromise).toHaveBeenCalledTimes(1);
+    expect(componentIndexPromise).toHaveBeenCalledWith(name, prog);
+
+    expect(componentPromise).toHaveBeenCalledTimes(1);
+    expect(componentPromise).toHaveBeenCalledWith(name, prog, style, quotes);
+
+    expect(styleSheetPromise).toHaveBeenCalledTimes(1);
+    expect(styleSheetPromise).toHaveBeenCalledWith(name, style);
+  });
+
+  it('generates component without styles', async () => {
+    (config.get as jest.Mock)
+      .mockReturnValueOnce({ ...globalConfig, skipStyles: true })
+      .mockReturnValueOnce(componentConfig);
+
+    mockResolvedPromises(promises, promiseSuccess);
+
+    await expect(generateComponent()).resolves.toEqual([promiseSuccess, promiseSuccess]);
+
+    expect(config.get).toHaveBeenCalledTimes(2);
+    expect(config.get).toHaveBeenNthCalledWith(1, Configs.Global);
+    expect(config.get).toHaveBeenNthCalledWith(2, Configs.Component);
+
+    expect(componentIndexPromise).toHaveBeenCalledTimes(1);
+    expect(componentIndexPromise).toHaveBeenCalledWith(name, prog);
+
+    expect(componentPromise).toHaveBeenCalledTimes(1);
+    expect(componentPromise).toHaveBeenCalledWith(name, prog, style, quotes);
+
+    expect(styleSheetPromise).not.toHaveBeenCalled();
+  });
+
+  it('rejects with error', async () => {
+    (config.get as jest.Mock).mockReturnValueOnce(globalConfig).mockReturnValueOnce(componentConfig);
+
+    mockRejectedPromises(promises, promiseError);
+
+    await expect(generateComponent()).rejects.toEqual(promiseError);
+
+    expect(config.get).toHaveBeenCalledTimes(2);
+    expect(config.get).toHaveBeenNthCalledWith(1, Configs.Global);
+    expect(config.get).toHaveBeenNthCalledWith(2, Configs.Component);
+
+    expect(componentIndexPromise).toHaveBeenCalledTimes(1);
+    expect(componentIndexPromise).toHaveBeenCalledWith(name, prog);
+
+    expect(componentPromise).toHaveBeenCalledTimes(1);
+    expect(componentPromise).toHaveBeenCalledWith(name, prog, style, quotes);
+
+    expect(styleSheetPromise).toHaveBeenCalledTimes(1);
+    expect(styleSheetPromise).toHaveBeenCalledWith(name, style);
+  });
+});
---
+++ src/commands/generateContext.spec.ts
@@ -0,0 +1,99 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+import { mockRejectedPromises, mockResolvedPromises } from '../../__mocks__/mockGenerationPromises';
+import config from '../config';
+
+import generateContext from './generateContext';
+import { contextIndexPromise, contextPromise, contextTypesPromise, providerPromise, reducerPromise } from './promises';
+
+import { Configs } from '../enums';
+
+jest.mock('../config', () => ({
+  __esModule: true,
+  default: { get: jest.fn() },
+}));
+
+jest.mock('./promises', () => ({
+  __esModule: true,
+  contextIndexPromise: jest.fn(),
+  contextPromise: jest.fn(),
+  contextTypesPromise: jest.fn(),
+  providerPromise: jest.fn(),
+  reducerPromise: jest.fn(),
+}));
+
+jest.mock('../utils', () => ({
+  __esModule: true,
+  makeDir: jest.fn(),
+}));
+
+const promises = [contextIndexPromise, contextPromise, contextTypesPromise, providerPromise, reducerPromise];
+
+describe('generateContext', () => {
+  const name = 'foo';
+  const prog = 'baz';
+
+  const promiseSuccess = 'corge';
+  const promiseError = 'waldo';
+
+  const globalConfig = {
+    prog,
+  };
+
+  const contextConfig = {
+    name,
+  };
+
+  it('generates context', async () => {
+    (config.get as jest.Mock).mockReturnValueOnce(globalConfig).mockReturnValueOnce(contextConfig);
+
+    mockResolvedPromises(promises, promiseSuccess);
+
+    await expect(generateContext()).resolves.toEqual(promises.map(() => promiseSuccess));
+
+    expect(config.get).toHaveBeenCalledTimes(2);
+    expect(config.get).toHaveBeenNthCalledWith(1, Configs.Global);
+    expect(config.get).toHaveBeenNthCalledWith(2, Configs.Context);
+
+    expect(contextPromise).toHaveBeenCalledTimes(1);
+    expect(contextPromise).toHaveBeenCalledWith(name, prog);
+
+    expect(contextIndexPromise).toHaveBeenCalledTimes(1);
+    expect(contextIndexPromise).toHaveBeenCalledWith(name, prog);
+
+    expect(contextTypesPromise).toHaveBeenCalledTimes(1);
+    expect(contextTypesPromise).toHaveBeenCalledWith(name, prog);
+
+    expect(providerPromise).toHaveBeenCalledTimes(1);
+    expect(providerPromise).toHaveBeenCalledWith(name, prog);
+
+    expect(reducerPromise).toHaveBeenCalledTimes(1);
+    expect(reducerPromise).toHaveBeenCalledWith(name, prog);
+  });
+
+  it('rejects with error', async () => {
+    (config.get as jest.Mock).mockReturnValueOnce(globalConfig).mockReturnValueOnce(contextConfig);
+
+    mockRejectedPromises(promises, promiseError);
+
+    await expect(generateContext()).rejects.toEqual(promiseError);
+
+    expect(config.get).toHaveBeenCalledTimes(2);
+    expect(config.get).toHaveBeenNthCalledWith(1, Configs.Global);
+    expect(config.get).toHaveBeenNthCalledWith(2, Configs.Context);
+
+    expect(contextPromise).toHaveBeenCalledTimes(1);
+    expect(contextPromise).toHaveBeenCalledWith(name, prog);
+
+    expect(contextIndexPromise).toHaveBeenCalledTimes(1);
+    expect(contextIndexPromise).toHaveBeenCalledWith(name, prog);
+
+    expect(contextTypesPromise).toHaveBeenCalledTimes(1);
+    expect(contextTypesPromise).toHaveBeenCalledWith(name, prog);
+
+    expect(providerPromise).toHaveBeenCalledTimes(1);
+    expect(providerPromise).toHaveBeenCalledWith(name, prog);
+
+    expect(reducerPromise).toHaveBeenCalledTimes(1);
+    expect(reducerPromise).toHaveBeenCalledWith(name, prog);
+  });
+});
