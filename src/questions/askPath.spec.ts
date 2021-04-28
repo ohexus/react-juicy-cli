16c78a2df14e4a80da8c38dd4e07cb88b610f09b
ohexus <moskalenko.taras.d@gmail.com>
2021-04-29 00:52:15 +0300

    added askPath question

---
+++ src/questions/askPath.spec.ts
@@ -0,0 +1,25 @@
+import inquirer from 'inquirer';
+import askPath from './askPath';
+
+jest.mock('inquirer', () =>
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  Object.assign(jest.requireActual('inquirer'), {
+    prompt: jest.fn(),
+  }),
+);
+
+describe('askPath', () => {
+  it('returns path', async () => {
+    const path = 'foo';
+
+    ((inquirer.prompt as unknown) as jest.Mock).mockResolvedValue({ path });
+
+    expect(await askPath()).toEqual(path);
+  });
+
+  it('returns null', async () => {
+    ((inquirer.prompt as unknown) as jest.Mock).mockResolvedValue({ path: '' });
+
+    expect(await askPath()).toBeNull();
+  });
+});
---
+++ src/questions/askPath.ts
@@ -0,0 +1,22 @@
+import inquirer from 'inquirer';
+
+import { GenerationEntities } from '../enums';
+import { QuestionReply } from '../interfaces';
+
+const pathQuestion = (entity: GenerationEntities | 'entity') => ({
+  name: 'path',
+  type: 'input',
+  message: `Please type a path where to generate ${entity} or leave an empty string to generate ${entity} right here:`,
+});
+
+async function askPath(entity?: GenerationEntities): Promise<string | null> {
+  const dirPath = ((await inquirer.prompt([pathQuestion(entity || 'entity')])) as QuestionReply<string>).path;
+
+  if (!dirPath || !dirPath.length) {
+    return null;
+  }
+
+  return dirPath;
+}
+
+export default askPath;
