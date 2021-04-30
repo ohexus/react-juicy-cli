eead9af81be0befceb8f716f7f6de3d4037dd317
ohexus <moskalenko.taras.d@gmail.com>
2021-04-30 17:24:16 +0300

    added FC to the imports from react

--- src/templates/components/componentTsTemplate.ts
+++ src/templates/components/componentTsTemplate.ts
@@ -2,11 +2,15 @@ import cssImportTemplate from './cssImportTemplate';
 
 import { Quotes, StyleLangs } from '../../enums';
 
-const componentTsTemplate = (name: string, cssExt: StyleLangs, quotes: Quotes): string => `import React from 'react';
+const componentTsTemplate = (
+  name: string,
+  cssExt: StyleLangs,
+  quotes: Quotes,
+): string => `import React, { FC } from 'react';
 ${cssImportTemplate(name, cssExt)}
-export interface ${name}Props {}
+export type ${name}Props = {};
 
-const ${name}: React.FC<${name}Props> = ({}) => (
+const ${name}: FC<${name}Props> = () => (
   <div className=${quotes}${name.toLowerCase()}${quotes} data-testid=${quotes}${name}.testId${quotes}>
     ${name} works!
   </div>
