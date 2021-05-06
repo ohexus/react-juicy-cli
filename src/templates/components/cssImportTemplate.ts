57799924a83f258c050ce3b79601ca12125e5188
Taras Moskalenko <moskalenko.taras.d@gmail.com>
2021-05-06 10:11:17 +0300

    Refactor/add import styles to css import (#31)
    * added styles import instead of direct .css import
    
    * v0.8.0

--- src/templates/components/cssImportTemplate.ts
+++ src/templates/components/cssImportTemplate.ts
@@ -2,7 +2,7 @@ import { StyleLangs } from '../../enums';
 
 const cssImportTemplate = (name: string, cssExt: StyleLangs): string => {
   if (cssExt && cssExt.length && cssExt !== StyleLangs.Skip) {
-    return '\n' + `import './${name}.${cssExt}';` + '\n';
+    return '\n' + `import styles from './${name}.${cssExt}';` + '\n';
   }
 
   return '';
